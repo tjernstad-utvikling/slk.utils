6a9a96fec3f9c42d0ee5e2525d0424d5ea001323
Ole Tjernstad <ott556@gmail.com>
2022-09-17 20:28:58 +0200

    add interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import data from "./checkpointCategories.json";
 
-export const categories = { ...data.categories };
+export const categories: Category[] = { ...data.categories };
+
+interface Category {
+  key: string;
+  name: string;
+  groups: {
+    key: number;
+    name: string;
+  }[];
+}
