0110129ec2d6dc04d80d4f4c350a544d86d52039
Ole Tjernstad <ott556@gmail.com>
2022-09-17 21:03:50 +0200

    export categories

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import data from "./checkpointCategories.json";
+
+export const categories: Category[] = [...data.categories];
+
+interface Category {
+  key: string;
+  name: string;
+  groups: {
+    key: number;
+    name: string;
+  }[];
+}
